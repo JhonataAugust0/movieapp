167da353cdeedaa480115684b49fb52f4cbc83c9
Jhonata Augusto <jhon.augustosilva@gmail.com>
2021-11-24 22:30:11 -0300

    Construção da home page
    Utilização do repository e do controller para a construção da home page através da requisição da API dio

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:movieapp/controllers/movie_controller.dart';
+import 'package:movieapp/models/movies_model.dart';
+import 'package:movieapp/repositories/movies_repository_imp.dart';
+import 'package:movieapp/service/dio_service_imp.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final MovieController _controller =
+      MovieController(MoviesRepositoryImp(DioServiceImp()));
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+        body: ValueListenableBuilder<Movies?>(
+            valueListenable: _controller.movies,
+            builder: (_, movies, __) {
+              return movies != null
+                  ? ListView.builder(
+                      itemCount: movies.ListMovies.length,
+                      itemBuilder: (_, idx) =>
+                          Text(movies.ListMovies[idx].title))
+                  : Container();
+            }));
   }
 }
