c330991dc2281bc79548eb5d4f1f45ab21bf45ad
Jhonata Augusto <jhon.augustosilva@gmail.com>
2021-11-24 21:42:48 -0300

    Estruturação do projeto
    Criação da estrutura dos models e services que servirão de base para a feitoria do aplicativo

---
+++ lib/service/dio_service_imp.dart
@@ -0,0 +1,13 @@
+import 'package:dio/dio.dart';
+import 'package:movieapp/service/dio_service.dart';
+
+class DioServiceImp implements DioService {
+  @override
+  Dio getDio() {
+    return Dio(BaseOptions(baseUrl: 'https://api.themoviedb.org/4', headers: {
+      'content-type': 'application/json;charset=utf-8',
+      'authorization':
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNDIyZGM4ZDhlMDI3OTllY2JkZDYwZTE0OTdmZGQ4MCIsInN1YiI6IjYxOWVjYmM5YzBhZTM2MDAyYjY4MmRjZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rZuvml_W6MDDpOsNCLc0wSSqAaPMJ-KEyhJswR7rGwU'
+    }));
+  }
+}
